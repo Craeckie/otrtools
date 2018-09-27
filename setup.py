1512cf06a7372139e0086ee334996dc3630db2dc
Craeckie <josia-internet@rosskopfs.de>
2018-09-27 18:23:41 +0200

    Added inital setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+# from distutils.core import setup
+import setuptools
+
+setuptools.setup(
+    name='OTRTools',
+    version='0.1',
+    url="N/A",
+    description="OTRTools",
+    packages=setuptools.find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+#     long_description=open('README.md').read(),
+#     long_description_content_type="text/markdown",
+
+    install_requires=[
+        "celery",
+        "django"
+    ]
+)
