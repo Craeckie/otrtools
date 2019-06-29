d111b9be813964dcfa02a00cecd436c5a52f3632
Craeckie <josia-internet@rosskopfs.de>
2019-06-29 20:50:36 +0200

    Broker: changed password

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo systemctl start rabbitmq.service
-python3 manage.py runserver &
-celery -A otrtools worker -l info --broker amqp://user:FzWcvA_rCCgq-V_hDXoIMM4ySYpecuz@localhost:5672/vhost
+#python3 manage.py runserver &
+celery -E -A otrtools worker -l info --broker amqp://my_user:ilgaOUGJpKi3hd_7EtUjlof9sp8yTrGeZqPZH_wTOYt2FVEOJs@localhost:5672/vhost
