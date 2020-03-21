d1ddae91031c35f14c06f2e8904feb4fdad84b94
root <root@sanebig>
2020-03-21 21:11:07 +0100

    make STATIC_ROOT configurable

--- otrtools/settings.py
+++ otrtools/settings.py
@@ -123,7 +123,7 @@ djcelery.setup_loader()
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.environ.get("STATIC_ROOT", os.path.join(BASE_DIR, 'static'))
 
 STATIC_URL = '/static/'
 
