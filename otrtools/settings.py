c294b04d6ae3157e95a6985f999080883cc23b52
Craeckie <josia-internet@rosskopfs.de>
2020-09-22 21:38:23 +0200

    Removed unneccesary config

--- otrtools/settings.py
+++ otrtools/settings.py
@@ -144,13 +144,6 @@ FORMAT_PRIORITIES = [
     'MP4',
 ]
 
-# Workers / Message Queue
-#CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost:5672//'
-CELERY_RESULT_BACKEND = 'amqp://my_user:YiMkX-8xwew_Uqp9m9_GGTkCMHMV7p3@localhost:5672/vhost'
-CELERY_BROKER_URL = 'amqp://my_user:YiMkX-8xwew_Uqp9m9_GGTkCMHMV7p3@localhost:5672/vhost'
-CELERY_BROKER_URL  = 'amqp://guest:guest@localhost:5672//'
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 CELERY_ACCEPT_CONTENT = ['pickle']
 CELERY_TASK_SERIALIZER = 'pickle'
