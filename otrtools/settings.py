627ef9a4310bb08524781da97b844cb5dccd51a8
root <josia-login@rosskopfs.de>
2022-02-09 13:02:24 +0100

    removed django_celery_results

--- otrtools/settings.py
+++ otrtools/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_celery_results',
     'downloader',
     'bootstrap4',
     'searcher',
